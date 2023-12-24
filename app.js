7c20ab82172277553bf8867d41cd660835c71cb3
Denis Nazarovs <denis.nazarovs@va.lv>
2023-12-24 18:34:49 +0200

    Server port change
    macOS compatibility

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var redirect_uri = CONFIG.redirect_uri; // Your redirect uri
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
-var generateRandomString = function(length) {
+var generateRandomString = function (length) {
   var text = '';
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
@@ -40,10 +40,10 @@ var stateKey = 'spotify_auth_state';
 var app = express();
 
 app.use(express.static(__dirname + '/public'))
-   .use(cors())
-   .use(cookieParser());
+  .use(cors())
+  .use(cookieParser());
 
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
 
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
@@ -60,13 +60,13 @@ app.get('/login', function(req, res) {
     }));
 });
 
-app.get('/logout', function(req, res) {
+app.get('/logout', function (req, res) {
   // redirect to spotify logout page
   res.redirect('https://www.spotify.com/logout/');
-  });
-  
+});
 
-app.get('/callback', function(req, res) {
+
+app.get('/callback', function (req, res) {
 
   // your application requests refresh and access tokens
   // after checking the state parameter
@@ -95,11 +95,11 @@ app.get('/callback', function(req, res) {
       json: true
     };
 
-    request.post(authOptions, function(error, response, body) {
+    request.post(authOptions, function (error, response, body) {
       if (!error && response.statusCode === 200) {
 
         var access_token = body.access_token,
-            refresh_token = body.refresh_token;
+          refresh_token = body.refresh_token;
 
         var options = {
           url: 'https://api.spotify.com/v1/me',
@@ -108,7 +108,7 @@ app.get('/callback', function(req, res) {
         };
 
         // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
+        request.get(options, function (error, response, body) {
           console.log(body);
         });
 
@@ -128,7 +128,7 @@ app.get('/callback', function(req, res) {
   }
 });
 
-app.get('/refresh_token', function(req, res) {
+app.get('/refresh_token', function (req, res) {
 
   // requesting access token from refresh token
   var refresh_token = req.query.refresh_token;
@@ -142,7 +142,7 @@ app.get('/refresh_token', function(req, res) {
     json: true
   };
 
-  request.post(authOptions, function(error, response, body) {
+  request.post(authOptions, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token;
       res.send({
@@ -153,4 +153,4 @@ app.get('/refresh_token', function(req, res) {
 });
 
 console.log('Listening on 5000');
-app.listen(5000);
+app.listen(8000);
